b0446a84cdba628b4ebfbe6e5bcaf5708825b84c
Suhendra <suhendra0812@gmail.com>
2020-06-05 16:44:46 +0800

    menambahkan fungsi interpolasi piecewise linear dan cubic hermit polynomial

--- ais_interpolation.py
+++ ais_interpolation.py
@@ -1,8 +1,32 @@
 import numpy as np
+import pandas as pd
 from datetime import datetime, timedelta
+from scipy.interpolate import interp1d, PchipInterpolator
 
 ais_sample = ais_filter_ori[ais_filter_ori['mmsi'] == 533130901]
 ais_sample = ais_sample[['time','longitude','latitude','sog','cog','heading']]
+
+t = (pd.to_datetime(ais_sample['time']) - pd.to_datetime('1970-01-01')).dt.total_seconds()
+lon = ais_sample['longitude']
+lat = ais_sample['latitude']
+
+t_intp = np.arange(t.iloc[0], t.iloc[len(t)-1], 10*60)
+
+L_lon = interp1d(t, lon)
+L_lat = interp1d(t, lat)
+
+P_lon = PchipInterpolator(t, lon)
+P_lat = PchipInterpolator(t, lat)
+
+lon_intp = []
+lat_intp = []
+for ti in t_intp:
+    lon_intp.append(P_lon(ti))
+    lat_intp.append(P_lat(ti))
+
+intp_gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(lon_intp, lat_intp))
+intp_layer = QgsVectorLayer(intp_gdf.to_json(), 'interpolasi', 'ogr')
+QgsProject.instance().addMapLayer(intp_layer)
 
 x1 = ais_sample.iloc[:3]['longitude']
 y1 = ais_sample.iloc[:3]['latitude']
